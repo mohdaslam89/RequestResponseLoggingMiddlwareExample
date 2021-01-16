8c75f847786fded8deafda36bc762d5a903d2a4c
Mohd Aslam <mohdaslam89@users.noreply.github.com>
2021-01-16 23:05:22 +0530

    Upload Request Response Logging Exampl
    DotNet Core 3.1

---
+++ MiddlewareExample/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MiddlewareExample.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MiddlewareExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        // GET api/<EmployeeController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Get(): Empoloyee method call";
+        }
+        // GET api/<EmployeeController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Employee> Get(int id)
+        {
+            var employee = new Employee()
+            {
+                ID = id,
+                FirstName = "firstName",
+                LastName = "lastName",
+                DateOfBirth = DateTime.Now.AddYears(-31)
+            };
+
+            return Ok(employee);
+        }
+    }
+}
